aeab557db2a734dd488d0e68d8046283de1d6374
razin-123 <razin.sachi123@gmail.com>
2024-12-24 22:53:17 +0600

    Update 1901002_Task07_Streamlit.py

--- 1901002_Task07_Streamlit.py
+++ 1901002_Task07_Streamlit.py
@@ -47,43 +47,43 @@ Site : SUNDARBANS, BAGERHAT, Bangladesh''')
         """Forest Abode is a residence located in Bagerhat, near the Sundarbans. The form is conceptualized through two interconnected volumes and courtyards, blending indoor and outdoor spaces. Sloped roofs, skylights, and strategic openings optimize natural light and airflow, addressing the tropical climate. Water bodies and indoor greenery create relaxing zones, while the foyer connects the formal living, guest bedroom, and dining areas, ensuring spatial continuity with views oriented toward the southern river. The design integrates design elements, such as facade extensions for storage, bamboo screens reflecting local architectural heritage. The family living area features double-height bamboo screens for shading, adding both verticality and openness. A sculptural staircase leads to two upper-level bedrooms with green terraces overlooking the forest and river. Emphasizing climate responsiveness, ergonomics, and cultural context, the design offers a comfortable, natural living experience.
         """
     )
-    display_image("https://github.com/razin-123/1901002_Task07/tree/main/Images/south elevation.png", "View from river", column_layout=1)
+    display_image("Images/south elevation.png", "View from river", column_layout=1)
 
 #Concept
 elif selected_section == "Concept":
     st.header("Conceptual Development")
     st.write("The form is designed as two forms connected by a intermediate space, with angles creating views towards river and forest, oriented towards south for optimum windflow and daylighting.")
-    display_image("images/concept.png", "Conceptual Development", column_layout=1)
+    display_image("Images/concept.png", "Conceptual Development", column_layout=1)
 
 
 #Plans
 elif selected_section == "Plans":
     st.header("Plans")
-    display_image("images/site plan.png", "Site Plan", column_layout=1)
-    display_image("images/plan.png", "Ground Floor Plan", column_layout=1)
+    display_image("Images/site plan.png", "Site Plan", column_layout=1)
+    display_image("Images/plan.png", "Ground Floor Plan", column_layout=1)
 
 #Elevations and Sections
 elif selected_section == "Elevations and Sections":
     st.header("Elevations and Sections")
     st.markdown("Elevations")
-    display_image("images/elevation.png", "Elevation", column_layout=1)
+    display_image("Images/elevation.png", "Elevation", column_layout=1)
     st.markdown("Sections")
-    display_image("images/section.png", "Section", column_layout=1)
+    display_image("Images/section.png", "Section", column_layout=1)
 
 #Exterior and Interior views
 elif selected_section == "Exterior and Interior views":
     st.header("Exterior and Interior views")
     st.markdown("Exterior views")
     display_image(
-    "images/east courtyard.png", "East courtyard", 
+    "Images/east courtyard.png", "East courtyard", 
     column_layout=2, 
-    image_path2="images/west courtyard.png", caption2="West courtyard"
+    image_path2="Images/west courtyard.png", caption2="West courtyard"
 )
     st.markdown("Interior views")
     display_image(
-    "images/dining.png", "Dining", 
+    "Images/dining.png", "Dining", 
     column_layout=2, 
-    image_path2="images/living.png", caption2="Living"
+    image_path2="Images/living.png", caption2="Living"
 )
 #Footer
 st.markdown("---")
